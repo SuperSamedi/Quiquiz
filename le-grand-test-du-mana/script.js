4b6103d061ecbe22035a3c3664b9a933cba1e66d
SuperSamedi <contact.max.dolmans@gmail.com>
2023-11-10 18:03:33 +0100

    Loading questions from json file + loading screen

--- le-grand-test-du-mana/script.js
+++ le-grand-test-du-mana/script.js
@@ -1,17 +1,46 @@
+const loadingScreen = document.getElementById("loading-screen");
+const quiz = document.getElementById("quiz");
+const questionCount = document.getElementById("question-count");
 const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choice"));
+const choices = Array.from(document.getElementsByClassName("choice-text"));
 
+let questions = [];
 let currentQuestion = {};
+let questionCounter = 0;
 let acceptingAnswer = false;
-let availableQuestions = [];
 
-const questions = new loadQuestions();
-console.log(questions);
 
-async function loadQuestions() {
-    const response = await fetch('questions.json', {
-        credentials: "same-origins",
+fetch("questions.json")
+    .then(response => {
+        // console.log(response);
+        return response.json();
+    })
+    .then(loadedQuestions => {
+        questions = loadedQuestions;
+        console.log(questions);
+        startQuiz();
+    })
+    .catch(error => {
+        // TODO: create a 500 code page
+        console.error(error);
     });
-    const questions = await response.json();
-    return questions;
-}
+
+
+startQuiz = () => {
+    questionCounter = 0;
+    getNextQuestion();
+    quiz.classList.remove("hidden");
+    loadingScreen.classList.add("hidden");
+};
+
+getNextQuestion = () => {
+    questionCounter++;
+    questionCount.innerText = `Question ${questionCounter} sur ${questions.length}`;
+    currentQuestion = questions[questionCounter - 1];
+    console.log(currentQuestion)
+    question.innerText = currentQuestion.questionText;
+    choices.forEach(choice => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion.choices[number - 1].choiceText;
+    });
+};
